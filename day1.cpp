1169b0c0a7d364564301122c2f91484fd163a43e
helloAmulya <internatomix@gmail.com>
2025-07-20 03:15:56 +0530

    loops

--- day1.cpp
+++ day1.cpp
@@ -4,14 +4,15 @@ using namespace std;
 int main()
 {
 
-    // int a = 5;
     // conditional statements
-
+    /*
+    // int a = 5;
     // cout << typeid(a).name() << endl;
     // cout << "Hello World1" << endl;
     char ch;
     cout << "enter a character : ";
     cin >> ch;
+    // we can print the ascii characters based on the alphabets or vice-versa
     // if (ch >= 'a' && ch <= 'z')
     if (ch >= 65 && ch <= 98)
 
@@ -21,10 +22,41 @@ int main()
     }
     // else
     // {
-    //     cout << "uppercase" << endl;
-    // }
+        //     cout << "uppercase" << endl;
+        // }
 
-    // we can print the ascii characters based on the alphabets or vice-versa
+    */
+
+    // loops
+
+    // 3 loops for / while/ do while, same can be done by each just usage and effectiveness of each is different
+
+    int count = 1;
+    cout << "using while loop :" << endl;
+    while (count <= 10)
+    {
+
+        cout << count << endl;
+        count++;
+    }
+    // needs all values to be true to execute once
+
+    cout << "using do-while loop :" << endl;
+    int co1 = 1;
+    do
+    {
+        cout << co1 << endl;
+        co1++;
+    } while (co1 <= 5);
+    // executes at least one time before verifying the condition
+
+    cout << "using for loop :" << endl;
+
+    for (int i = 1; i <= 5; i++)
+    {
+        cout << i << endl;
+    }
+    // most efficient loop, all condition check
 
     return 0;
 }
