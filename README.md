da67f8233fb9e6282b1eb3d67eef87cb2509ce34
helloAmulya <internatomix@gmail.com>
2025-07-20 03:01:50 +0530

    cnditional statements

---
+++ README.md
@@ -0,0 +1,3 @@
+# hello this is the nano way editor
+ 
+this is using vim
--- day1.cpp
+++ day1.cpp
@@ -1,7 +1,30 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    
+int main()
+{
+
+    // int a = 5;
+    // conditional statements
+
+    // cout << typeid(a).name() << endl;
+    // cout << "Hello World1" << endl;
+    char ch;
+    cout << "enter a character : ";
+    cin >> ch;
+    // if (ch >= 'a' && ch <= 'z')
+    if (ch >= 65 && ch <= 98)
+
+    {
+        cout << "lowercase" << endl;
+        // cout << ch << endl;
+    }
+    // else
+    // {
+    //     cout << "uppercase" << endl;
+    // }
+
+    // we can print the ascii characters based on the alphabets or vice-versa
+
     return 0;
 }
