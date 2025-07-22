cfcb2c6d0b10ec45af4da7ac8e47b96f6b45342c
helloAmulya <internatomix@gmail.com>
2025-07-23 05:26:34 +0530

    binomial factorails

--- day3.cpp
+++ day3.cpp
@@ -19,15 +19,47 @@ int fact(int n)
     return n * (fact(n - 1));
 }
 
+int sumOfdigits(int n)
+{
+    int sum = 0;
+    while (n > 0)
+    {
+        int digit = n % 10;
+        sum += digit;
+        n = n / 10;
+    }
+    return sum;
+}
+
+// binomial coefficient
+float binomial(int n, int r)
+{
+    return (float)fact(n) / (fact(r) * fact(n - r));
+}
+
 int main()
 {
     // functions basics
 
-    int n;
-    cout << "Enter a number :: ";
-    cin >> n;
+    // in context of c++ stack is one of the important data structures,
+    // the main() is stored in the stack frame (call stack)
 
-    cout << "sum is :: " << printSum(n) << endl;
-    cout << "factoraial upto " << n << "is :: " << fact(n) << endl;
+    // pass by value - copy of the arguments
+    // pass by reference - use &a,& etc or pointer *a
+
+    int n, r;
+
+    cout << "Enter numbers :: ";
+    cin >> n >> r;
+
+    cout << "sum upto " << n << " is :: " << printSum(n) << endl;
+    cout << "factoraial upto " << n << " is :: " << fact(n) << endl;
+    cout << "Binomial is :: " << binomial(n, r) << endl;
+
+    int num;
+    cout << "Enter another number :: ";
+    cin >> num;
+    cout << "sum of digits is :: " << sumOfdigits(num) << endl;
+
     return 0;
 }
