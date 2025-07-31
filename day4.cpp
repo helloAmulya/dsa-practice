d03ae2d606dfd351e757bfdd7bedd65e8d4ebd85
helloAmulya <internatomix@gmail.com>
2025-07-31 23:51:11 +0530

    more data types

--- day4.cpp
+++ day4.cpp
@@ -10,17 +10,35 @@ using namespace std;
 
     right (>>) and left shift (<<) operator, shifts the bit as directed
 
+    global and local scopes (variable) means the way we defined the variale, inside a function/block -> local scope, whole code, whole section -> global scope
+
+    data types:
+        int -> limited value upto : -(2)^31 to (2)^31 - 1 , 4bytes : 32bits
+
+                Type         | Size     | Value Range (Approximate)
+                -------------|----------|-------------------------------
+                short        | 2 bytes  | –(2)^15 to (2)^15 – 1
+                int          | 4 bytes  | –(2)^31 to (2)^31 – 1
+                long         | 4 or 8 B | ≥ int, often same as or larger than int
+                long long    | 8 bytes  | –(2)^63 to (2)^63 – 1
+                signed       | —        | Same as signed int (allows negatives)
+                unsigned     | —        | 0 to (2^n) – 1 (only positive values)
+
+
+
 */
 
 int main()
 {
 
-    int a = 4, b = 6;
-    cout << (a & b) << endl;
-    cout<<"left shifting a 2 times : "<<(a<<2)<<endl;
-    cout<<"right shifting a 2 times : "<<(a>>2)<<endl;
+    // int a = 4, b = 6;
+    // cout << (a & b) << endl;
+    // cout << "left shifting a 2 times : " << (a << 2) << endl;
+    // cout << "right shifting a 2 times : " << (a >> 2) << endl;
+
+    int c;
+    cout << "Enter a number : " << endl;
+    cin >> c;
 
     return 0;
-
-
 }
