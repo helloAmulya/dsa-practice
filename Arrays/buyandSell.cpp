ea231ecfcb1f8f36be02a5aaf18cfc5d35751737
helloAmulya <internatomix@gmail.com>
2025-08-14 03:51:54 +0530

    ques: pow(x,n) using binary

--- Arrays/buyandSell.cpp
+++ Arrays/buyandSell.cpp
@@ -1,29 +1,41 @@
 #include <iostream>
 using namespace std;
 
+
+ double myPow(double x, int n) {
+        if (n == 0)
+            return 1.0;
+        if (x == 0)
+            return 0.0;
+        if (x == 1)
+            return 1.0;
+        if (x == -1 && n % 2 == 0)
+            return 1.0;
+        if (x == -1 && n % 2 != 0)
+            return -1.0;
+
+        long binform = n;
+        if (n < 0) {
+            x = 1 / x;
+            binform = -binform;
+        }
+        double ans = 1;
+
+        while (binform > 0) {
+            if (binform % 2 == 1) {
+                ans *= x;
+            }
+            x *= x;
+            binform /= 2;
+        }
+        return ans;
+    }
+
 int main()
 {
     // binary exponentiation
     // calculate pow of x
-    int x = 3, n = 5;
-    int binform = n;
-    double ans =  1;
-    if (n < 0)
-    {
-        x = 1 / x;
-        binform = -binform;
-    }
-
-    while (binform > 0)
-    {
-        if (binform % 2 == 0)
-        {
-            ans *= x;
-        }
-        x *= x;
-        binform /= 2;
-    }
-    cout<<ans;
+    cout<<"The ans is :: "<<myPow(2,10)<<endl;;
 
     return 0;
 }
